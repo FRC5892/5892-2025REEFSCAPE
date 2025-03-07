d2e11379d5f9b96a26628e8e4bd843b24d54405b
Michael <117385299+mstatistics@users.noreply.github.com>
2025-03-07 13:42:00 -0600

    Update at 'Fri Mar 07 13:41:59 CST 2025'

--- src/main/java/frc/robot/subsystems/CoralEndEffector/CoralEndEffector.java
+++ src/main/java/frc/robot/subsystems/CoralEndEffector/CoralEndEffector.java
@@ -21,7 +21,7 @@ import org.littletonrobotics.junction.AutoLogOutput;
 
 public class CoralEndEffector extends SubsystemBase {
   private final LoggedTunableNumber intakeDutyCycle =
-      new LoggedTunableNumber("Coral/Intake Duty Cycle", 0.1);
+      new LoggedTunableNumber("Coral/Intake Duty Cycle", 0.15);
   private final LoggedTunableNumber outtakeDutyCycle =
       new LoggedTunableNumber("Coral/Outtake Duty Cycle", 0.25);
   private final DutyCycleOut dutyCycleOut = new DutyCycleOut(0).withEnableFOC(true);

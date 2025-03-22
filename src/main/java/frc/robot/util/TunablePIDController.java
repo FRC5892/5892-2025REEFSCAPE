b8abfbacd03ae404e812613f5607c5e9e043fc93
Michael <117385299+mstatistics@users.noreply.github.com>
2025-03-21 21:49:29 -0500

    finish TunablePIDController

--- src/main/java/frc/robot/util/TunablePIDController.java
+++ src/main/java/frc/robot/util/TunablePIDController.java
@@ -5,20 +5,36 @@
 package frc.robot.util;
 
 import com.pathplanner.lib.config.PIDConstants;
-
 import edu.wpi.first.math.controller.PIDController;
 
 /** Add your docs here. */
 public class TunablePIDController extends PIDController {
-    private final LoggedTunableNumber[] tunableNumbers;
-    public TunablePIDController(String key, PIDConstants constants) {
-        super(constants.kP, constants.kI,constants.kD);
-        super.setIZone(constants.iZone);
-        tunableNumbers = new LoggedTunableNumber[]{
-            new LoggedTunableNumber(key+"/kP", constants.kP),
-            new LoggedTunableNumber(key+"/kP", constants.kP),
-            new LoggedTunableNumber(key+"/kP", constants.kP),
-        }
+  private final LoggedTunableNumber[] tunableNumbers;
 
-    }
+  public TunablePIDController(String key, PIDConstants constants, double period) {
+    super(constants.kP, constants.kI, constants.kD, period);
+    super.setIntegratorRange(-constants.iZone, constants.iZone);
+    tunableNumbers =
+        new LoggedTunableNumber[] {
+          new LoggedTunableNumber(key + "/kP", constants.kP),
+          new LoggedTunableNumber(key + "/kI", constants.kI),
+          new LoggedTunableNumber(key + "/kD", constants.kD),
+        };
+  }
+
+  public TunablePIDController(String key, PIDConstants constants) {
+    this(key, constants, 0.02);
+  }
+
+  public void updateConstants(Object id, PIDController... tuningFollowers) {
+    LoggedTunableNumber.ifChanged(
+        id,
+        constants -> {
+          super.setPID(constants[0], constants[1], constants[2]);
+          for (PIDController follower : tuningFollowers) {
+            follower.setPID(constants[0], constants[1], constants[2]);
+          }
+        },
+        tunableNumbers);
+  }
 }

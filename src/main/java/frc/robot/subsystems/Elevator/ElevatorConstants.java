b3e15a15bafb5f7e280317035dbdd0e7fb5facb9
Michael <117385299+mstatistics@users.noreply.github.com>
2025-03-30 11:27:00 -0500

    Update at 'Sun Mar 30 11:27:00 CDT 2025'

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
@@ -27,7 +27,7 @@ public class ElevatorConstants {
   public enum ElevatorPosition {
     INTAKE(new LoggedTunableMeasure<>("Elevator/Intake", Meters.mutable(0.0))),
     L2(new LoggedTunableMeasure<>("Elevator/L2", Meters.mutable(0.28))),
-    L3(new LoggedTunableMeasure<>("Elevator/L3", Meters.mutable(0.7))),
+    L3(new LoggedTunableMeasure<>("Elevator/L3", Meters.mutable(0.68))),
     L4(new LoggedTunableMeasure<>("Elevator/L4", Meters.mutable(1.265)));
 
     public final Supplier<MutDistance> height;
